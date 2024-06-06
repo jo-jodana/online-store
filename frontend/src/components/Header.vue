41a40d7b6ad235f0c3fb3cfb1bac007c26a47408
Nurdana <temabeu@icloud.com>
2024-06-07 00:25:32 +0500

    make header

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,35 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup>
+
+</script>
+
+<template>
+  <div class="w-full h-14 flex flex-row justify-between items-center">
+    <a href="/" class="text-[52px]">FASCO</a>
+    <ul class="w-[60%] flex flex-row justify-around items-center">
+      <li class="text-[#484848] text-[16px] ">
+        <a href="/">Home</a>
+      </li>
+      <li class="text-[#484848] text-[16px]">
+        <a href="/">Deals</a>
+      </li>
+      <li class="text-[#484848] text-[16px]">
+        <a href="/">New Arrivals</a>
+      </li>
+      <li class="text-[#484848] text-[16px]">
+        <a href="/">Packages</a>
+      </li>
+      <li class="text-[#484848] text-[16px] ">
+        <a href="/">Sign in</a>
+      </li>
+      <li class="bg-black text-white h-14 w-[152px] rounded-[10px] flex items-center justify-center hover:drop-shadow-2xl">
+        <a href="/">Sign Up</a>
+      </li>
+
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
